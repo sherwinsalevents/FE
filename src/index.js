654e46e323a7391e3ab7faf98a9d950eaf7c4eaf
Rene Boy Andoy <45546823+Rbandoy@users.noreply.github.com>
2026-04-30 14:39:14 +0800

    dummy push

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './redux-saga/store';
-
+     
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
